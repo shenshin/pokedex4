ebaebddb897283d89d7f9c6c2a7871884dd5dd14
Alexander Shenshin <shenshin@me.com>
2017-07-13 01:15:46 +0300

    Initial Commit

---
+++ pokedex4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex4
+//
+//  Created by Алик Базин on 13.07.2017.
+//  Copyright © 2017 Ales Shenshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
