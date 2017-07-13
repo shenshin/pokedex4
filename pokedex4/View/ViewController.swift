c77a4b1ef29b806e5e62255d2e291cb3c5a2396a
Alexander Shenshin <shenshin@me.com>
2017-07-13 04:40:54 +0300

    Создание функций для отображения CollectionView в классе ViewConroller

---
+++ pokedex4/View/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  pokedex4
+//
+//  Created by Алик Базин on 13.07.2017.
+//  Copyright © 2017 Ales Shenshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collection: UICollectionView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collection.dataSource = self
+        collection.delegate = self
+        
+        let charmander = Pokemon(name: "Charmander", pokedexID: 4)
+        print(charmander.name, " ", charmander.pokedexID)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 105, height: 105)
+    }
+}
+
