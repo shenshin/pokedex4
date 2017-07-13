a8087e54be61a0842538e5dac66415126b593947
Alexander Shenshin <shenshin@me.com>
2017-07-13 06:12:33 +0300

    Удаление лишних и добавление недостающих изображений покемонов

--- pokedex4/View/ViewController.swift
+++ pokedex4/View/ViewController.swift
@@ -25,6 +25,10 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            
+            let pokemon = Pokemon(name: "Pokémon", pokedexID: indexPath.row)
+            cell.configureCell(pokemon: pokemon)
+                        
             return cell
         } else {
             return UICollectionViewCell()
