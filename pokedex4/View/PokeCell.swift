ebe3f087632f9d2de6166d402996c07d06f0cd98
Alexander Shenshin <shenshin@me.com>
2017-07-13 03:14:23 +0300

    Создание класса PokeCell

---
+++ pokedex4/View/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  pokedex4
+//
+//  Created by Алик Базин on 13.07.2017.
+//  Copyright © 2017 Ales Shenshin. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+        self.nameLbl.text = self.pokemon.name.capitalized
+        self.thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+}
